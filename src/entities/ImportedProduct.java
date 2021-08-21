ee7ab4aef71d2c98fb515a81e8b40a110bb6a32e
leninepestana <leninepestana@gmail.com>
2021-08-22 00:33:31 +0100

    InheritanceExercise5 V1

---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class ImportedProduct extends Product {
+	private Double customFee;
+
+	public ImportedProduct() {
+		super();
+	}
+
+	public ImportedProduct(String name, Double price, Double customFee) {
+		super(name, price);
+		this.customFee = customFee;
+	}
+
+	public Double getCustomFee() {
+		return customFee;
+	}
+
+	public void setCustomFee(Double customFee) {
+		this.customFee = customFee;
+	}
+	
+	public double totalPrice() {
+		return price + customFee;
+	}
+
+	@Override
+	public String priceTag() {
+		return getName() 
+				+ String.format(" $ %.2f", totalPrice()) 
+				+ " (Customs fee: $ " + String.format("%.2f", getCustomFee()) + ")";
+	}
+	
+	
+	
+	
+}
